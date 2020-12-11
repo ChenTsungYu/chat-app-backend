040ffb64d111da92fc75001127f482640ae33e63
ChenTsungYu <z3337059@gmail.com>
2020-12-11 18:19:39 +0800

    modify CORS domain

--- app.js
+++ app.js
@@ -1,32 +1,18 @@
 const express = require('express');
 const config = require('./config/app');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 const http = require('http');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // implement cors; keep it before any route
-// app.use(cors()); 
-// Access-Control-Allow-Origin: *
-//app.options('*', cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", req.headers.origin);
-    // res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, DELETE, PATCH");
-//    if (req.method === "OPTIONS") {
-//               res.header(
-//                   "Access-Control-Allow-Methods",
-//                   "PUT, POST, PATCH, GET, DELETE"
-//               );
-//               return res.status(200).json({});
-//    }; 
     next();
 });
- // implement cors (better case) : app.use(cors("Frontend URL")); 
-//  app.use(cors(" URL")); 
 const router = require('./router');
 app.use(router);
 // static file root path
